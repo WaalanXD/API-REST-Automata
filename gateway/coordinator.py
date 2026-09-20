bf80c90ca92050b45e65279684d814de1a36b319
LukePaladis1 <salobedoya40@gmail.com>
2026-09-20 11:22:57 -0500

    Update coordinator.py

--- gateway/coordinator.py
+++ gateway/coordinator.py
@@ -1,4 +1,4 @@
-from functions.automata_logic import nfa_a_dfa, simular_dfa
+from functions.automata_logic import nfa_a_dfa, simular_dfa, minimize_dfa
 
 class AutomataGateway:
     @staticmethod
@@ -13,3 +13,25 @@ class AutomataGateway:
             return simular_dfa(dfa,cadena)
         except Exception as e:
             return {"error": "Internal error while executing the simulation.","detail": str(e)}
+    @staticmethod
+    def procesar_minimizacion(carga: dict) -> dict:
+        try:
+            n = carga.get("n")
+            alphabet = carga.get("alphabet")
+            final_states = carga.get("final_states")
+            transitions = carga.get("transitions")
+            
+            equivalent_pairs = minimize_dfa(n, alphabet, final_states, transitions)
+            
+            # Formatear la salida adecuadamente
+            formatted_pairs = [[p, q] for p, q in equivalent_pairs]
+            
+            return {
+                "equivalent_states": formatted_pairs,
+                "total_pairs": len(formatted_pairs)
+            }
+        except Exception as e:
+            return {
+                "error": "Internal error while executing the minimization algorithm.",
+                "detail": str(e)
+            }
